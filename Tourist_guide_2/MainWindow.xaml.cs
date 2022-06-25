ba095804d8500a139fd9c13ffe8deba1bc22756e
Ionina34 <igoninadasha0315@mail.ru>
2022-06-25 17:24:01 +0500

    Add project files.

---
+++ Tourist_guide_2/MainWindow.xaml.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Tourist_guide_2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<string> path;
+        Process pr;
+        public MainWindow()
+        {
+            InitializeComponent();
+            path = new List<string>();
+            path.Add("D:\\ДАША)))\\C# HomeWork\\Tourist_guide");
+            pr = new Process();
+            BackToolStrip.IsEnabled = false;
+            SerchDick();
+
+            Catalog.MouseDoubleClick += DoubleClick_listBox1;
+            BackToolStrip.Click += Back_Click;
+            OpenToolStrip.Click += Open_Click;
+
+        }
+        private void Open_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (Catalog.SelectedIndex != -1)
+                {
+                    path.Add(Catalog.SelectedItem.ToString());
+                    BackToolStrip.IsEnabled = true;
+                    var result = String.Join("\\", path.ToArray());
+                    if (File.Exists(result))
+                    {
+                        pr.StartInfo.FileName = result;
+                        pr.StartInfo.WorkingDirectory = System.IO.Path.GetDirectoryName(result);
+                        pr.Start();
+                    }
+                    else
+                        SerchFile();
+                }
+            }
+            catch (Exception) { }
+        }
+        private void Back_Click(object sender, RoutedEventArgs e)
+        {
+            if (path.Count == 1)
+                return;
+            if (path.Count == 2)
+            {
+                path.RemoveAt(path.Count - 1);
+                Catalog.Items.Clear();
+                SerchDick();
+                BackToolStrip.IsEnabled = false;
+            }
+            else
+            {
+                path.RemoveAt(path.Count - 1);
+                SerchFile();
+            }
+        }
+        private void DoubleClick_listBox1(object sender, RoutedEventArgs e)
+        {
+            if (Catalog.SelectedIndex != -1)
+            {
+                path.Add(Catalog.SelectedItem.ToString());
+                BackToolStrip.IsEnabled = true;
+                var result = String.Join("\\", path.ToArray());
+                if (File.Exists(result))
+                {
+                    pr.StartInfo.FileName = result;
+                    pr.StartInfo.WorkingDirectory = System.IO.Path.GetDirectoryName(result);
+                    pr.Start();
+                }
+                else
+                    SerchFile();
+            }
+        }
+        public void SerchDick()
+        {
+            DirectoryInfo dr = new DirectoryInfo("D:\\ДАША)))\\C# HomeWork\\Tourist_guide");
+            foreach (var d in dr.GetDirectories())
+            {
+                Catalog.Items.Add(d.Name);
+            }
+            foreach (var d in dr.GetFiles())
+            {
+                Catalog.Items.Add(d.Name);
+            }
+        }
+        public void SerchFile()
+        {
+            Catalog.Items.Clear();
+            var result = String.Join("\\", path.ToArray());
+            DirectoryInfo dr = new DirectoryInfo(result);
+            foreach (var d in dr.GetDirectories())
+            {
+                Catalog.Items.Add(d.Name);
+            }
+            foreach (var d in dr.GetFiles())
+            {
+                Catalog.Items.Add(d.Name);
+            }
+        }
+        private void Add_Click(object sender, RoutedEventArgs e)
+        {
+            int openWin = 0;
+            foreach (var Window in App.Current.Windows)
+                openWin += 1;
+
+            if (openWin == 1)
+            {
+                Window1 window = new Window1();
+                window.Show();
+            }
+            else return;
+        }
+    }
+}
---
+++ Tourist_guide_2/Window1.xaml.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.IO;
+using Microsoft.Win32;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Tourist_guide_2
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        string[] filePath;
+        string text;
+        public Window1()
+        {
+            InitializeComponent();
+            Picture.GotFocus += Box_GotFocus;
+            Picture.LostFocus += Box_LostFocus;
+            Video.GotFocus += Box_GotFocus;
+            Video.LostFocus += Box_LostFocus;
+        }
+        private void Box_GotFocus(object sender, RoutedEventArgs e)
+        {
+            TextBox box = sender as TextBox;
+            box.Clear();
+        }
+        private void Box_LostFocus(object sender, RoutedEventArgs e)
+        {
+            TextBox box = sender as TextBox;
+            box.BorderBrush = Brushes.Black;
+            if (box.Text == "")
+            {
+                box.Text = "Введите кол-во картинок";
+                return;
+            }
+
+            if (!(int.TryParse(box.Text, out int value)) || value < 0)
+            {
+                box.Text = "Должны быть только цифры";
+                box.BorderBrush = Brushes.Red;
+            }
+
+        }
+
+        private void Save_Click(object sender, RoutedEventArgs e)
+        {
+            if (Point.Text != "" && Picture.Text != "" && Description.Text != "" && Currency.Text != ""
+                && Price.Text != "")
+            {
+                for (int i = 0; i < filePath.Length; i++)
+                {
+                    //Bitmap btm = new Bitmap();
+                }
+            }
+            else
+            {
+                Point.BorderBrush = Brushes.Red;
+                Picture.BorderBrush = Brushes.Red;
+                Description.BorderBrush = Brushes.Red;
+                Currency.BorderBrush = Brushes.Red;
+                Price.BorderBrush = Brushes.Red;
+            }
+        }
+
+        private void btnPicture_Click(object sender, RoutedEventArgs e)
+        {
+            string str = Picture.Text;
+            int num = int.Parse(str);
+            filePath = new string[num];
+
+            for (int i = 0; i < num; i++)
+            {
+                OpenFileDialog open = new OpenFileDialog();
+                open.Filter = "Image files (*.png;*.jpg)|*.png;*.jpg";
+
+                if (open.ShowDialog() == true)
+                    filePath[i] = open.FileName;
+            }
+            Picture.Text = "Картинки загружены";
+        }
+
+        private void Picture_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            TextBox box = sender as TextBox;
+            if (box.Text == "")
+                text = "";
+            if (int.TryParse(box.Text, out int value) && value > 0)
+                text = box.Text;
+            else
+            {
+                box.Text = text;
+                box.CaretIndex = box.Text.Length;
+            }
+        }
+    }
+}
